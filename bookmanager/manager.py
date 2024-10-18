e31297695392bb8dd81f0aa4f5b0b73854392034
Paspenta <paspenta@outlook.jp>
2024-10-18 14:28:08 +0900

    recreate manage.py

--- bookmanager/manager.py
+++ bookmanager/manager.py
@@ -1,18 +1,8 @@
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, url_for
 )
+from werkzeug.exceptions import abort
+
 from bookmanager.db import get_db
 
-bp = Blueprint("manager", __name__, url_prefix="/index")
-
-@bp.route("/")
-def index():
-    db = get_db()
-
-
-    return render_template("index.html",
-                            title="index",
-                            outers=outers,
-                            Publishers=Publishers,
-                            have_sites=have_sites,
-                            books=books)
+bp = Blueprint("manager", __name__)
