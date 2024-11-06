ff479b6f48314e6384054424b05feeee02a36eb1
Paspenta <paspenta@outlook.jp>
2024-11-06 10:22:49 +0900

    add base conftest

---
+++ tests/conftest.py
@@ -0,0 +1,37 @@
+import os
+import tempfile
+
+import pytest
+from bookmanager import create_app
+from bookmanager.db import get_db, init_db
+
+# テストDB初期値をINSERTするSQLファイルをutf8でデコードして_data_sqlに格納
+with open(os.path.join(os.path.dirname(__file__), "data.sql"), "rb") as f:
+    _data_sql = f.read().decode("utf8")
+
+@pytest.fixture
+def app():
+    db_fd, db_path = tempfile.mkstemp()
+
+    # appをテストモード, dbにtempfileを指定
+    app = create_app({
+        "TESTING": True,
+        "DATABASE": db_path
+    })
+
+    with app.app_context():
+        init_db()
+        get_db().executescript(_data_sql)
+    
+    yield app
+
+    os.close(db_fd)
+    os.unlink(db_path)
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
+
+@pytest.fixture
+def runner(app):
+    return app.test_cli_runner()
