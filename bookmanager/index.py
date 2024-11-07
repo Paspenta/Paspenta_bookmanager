4aacff482be1d28dac616677e6187d53710a2a0f
Paspenta <paspenta@outlook.jp>
2024-11-07 17:33:02 +0900

    split func index

--- bookmanager/index.py
+++ bookmanager/index.py
@@ -10,10 +10,14 @@ from .manager import (
     bp, get_page
 )
 
+GET_BOOK_AMOUNT = 30
+GET_SERIES_AMOUNT = 15
+
 def get_url_parameters():
     """_summary_
+
     URL Parameters
-    ----------
+    --------------
     SeriesName: str
         シリーズ名から検索する
     AuthorName: str
@@ -25,41 +29,95 @@ def get_url_parameters():
 
     Returns
     -------
-    ret: dict
+    parms: dict
         URLパラメタ
     """
-    ret = {
+    parms = {
         "Title": "%" + request.args.get("Title", "") + "%",
         "SeriesName": "%" + request.args.get("SeriesName", "") + "%",
         "AuthorName": "%" + request.args.get("AuthorName", "") + "%",
         "PublisherName": "%" + request.args.get("PublisherName", "") + "%",
-        "LocationName": "%" + request.args.get("LocationName", "") + "%"
+        "LocationName": "%" + request.args.get("LocationName", "") + "%",
+        "page":get_page(request.args.get("Page", "0"))
     }
-    return ret
+    return parms
 
 
-@bp.route("/")
-@login_required
-def index():
-    """Overview
-    本をシリーズごとに表示
+def get_have_books(db, UserID, parms):
+    books = db.execute(
+        """
+        SELECT
+            Books.BookID AS BookID,
+            Title,
+            Series.SeriesName AS SeriesName,
+            Locations.LocationName AS LocationName,
+            COALESCE(Publishers.PublisherName, '') AS PublisherName,
+            COALESCE(GROUP_CONCAT(Authors.AuthorName, ','), '') AS Authors,
+            COALESCE(PublicationDate, '') AS PublicationDate,
+            COALESCE(ISBN13, ISBN10, '') AS ISBN
+        FROM Books
+        JOIN Series ON Series.SeriesID = Books.SeriesID
+        LEFT JOIN Locations ON Locations.LocationID = Books.LocationID
+        LEFT JOIN Publishers ON Publishers.PublishersID = Books.PublishersID
+        LEFT JOIN BookAuthors ON Books.BookID = BookAuthors.BookID
+        LEFT JOIN Authors ON Authors.AuthroID = BookAuthors.AuthorsID
+        WHERE
+            Books.UserID = ?
+            AND Title LIKE ?
+            AND `LocationName` LIKE ?
+            AND `Authors` LIKE` ?
+        ORDER BY Title
+        LIMIT ? OFFSET ?
+        """, (
+            UserID, parms["Title"], parms["LocationName"], parms["Author"],
+            GET_BOOK_AMOUNT, GET_BOOK_AMOUNT * parms["page"]
+        )
+    ).fetchall()
 
-    Returns
-    -------
-    検索条件がある場合、検索条件に一致する本を表示
-    検索条件がない場合、ユーザーが持つ本を表示
-    """
-    db = get_db()
-    UserID = g.user["UserID"]
+def get_series_data(row, db, Title):
+    series_data = dict(row)
+    # 本がある場所を追加
+    series_data["Locations"] = db.execute(
+        """
+        SELECT
+            Locations.LocationName AS LocationName,
+            COUNT(Books.LocationID) AS VolumeCount
+        FROM Books
+        JOIN Locations ON Books.LocationID = Locations.LocationID
+        WHERE SeriesID = ?
+        GROUP BY Books.LocationID
+        ORDER BY `VolumeCount` DESC;
+        """, (series_data["SeriesID"],)
+    ).fetchall()
 
-    page = request.args.get("Page", "0")
-    page = get_page(page)
+    series_data["volumes"] = db.execute(
+        """
+        SELECT
+            BookID,
+            Title,
+            COALESCE(PublicationDate, "") AS PublicationDate,
+            Locations.LocationName AS LocationName,
+            COALESCE(ISBN13, ISBN10, "") AS ISBN
+        FROM Books
+        JOIN Locations ON Books.LocationID = Locations.LocationID
+        WHERE SeriesID = ? AND Title LIKE ?
+        ORDER BY Title;
+        """, (series_data["SeriesID"], Title)
+    ).fetchall()
 
-    plus_parms = {**request.args}
-    minus_parms = {**request.args}
-    plus_parms["Page"] = page+1
-    minus_parms["Page"] = page-1 if page>0 else 0
+    parms = {
+        "title":series_data["SeriesName"],
+        "seriesName":series_data["SeriesName"],
+        "publisher":series_data["PublisherName"],
+        "author":series_data["Authors"],
+        "Location":series_data["Locations"][0]["LocationName"]
+    }
+    series_data["add_volume_url"] = url_for('manager.register', **parms)
 
+    return series_data
+
+
+def get_series(db, UserID, parms):
     series_list = db.execute(
         """
         SELECT
@@ -80,71 +138,64 @@ def index():
         HAVING
             `PublisherName` LIKE ?
             AND `Authors` LIKE ?
-        LIMIT 15 OFFSET ?;
+        ORDER BY SeriesName
+        LIMIT ? OFFSET ?;
         """, (
-        UserID, SeriesName, Title, PublisherName, AuthorName, page)
+            UserID,
+            parms["SeriesName"], parms["Title"], parms["PublisherName"], parms["AuthorName"],
+            GET_SERIES_AMOUNT, parms["page"] * GET_SERIES_AMOUNT
+        )
     ).fetchall()
 
-    # series_listをsqlite.rowオブジェクトからdictに変換
-    series_list = [dict(row) for row in series_list]
+    # 各シリーズに情報を追加
+    series_list = [get_series_data(row, db, parms["Title"]) for row in series_list]
+    return series_list
 
-    for i in range(len(series_list)): # シリーズごとの情報
-        series_data = series_list[i]
-        add_data = dict()
-        # 本がある場所を追加
-        add_data["Locations"] = db.execute(
-            """
-            SELECT
-                Locations.LocationName AS LocationName,
-                COUNT(Books.LocationID) AS VolumeCount
-            FROM Books
-            JOIN Locations ON Books.LocationID = Locations.LocationID
-            WHERE SeriesID = ?
-            GROUP BY Books.LocationID
-            ORDER BY `VolumeCount` DESC;
-            """, (series_data["SeriesID"],)
-        ).fetchall()
-        authors = db.execute(
-            """
-            WITH SeriesAuthors AS (
-                SELECT AuthorID
-                FROM BookAuthors
-                WHERE SeriesID = ?
-            )
-            SELECT AuthorName
-            FROM Authors
-            WHERE AuthorID IN SeriesAuthors;
-            """, (series_data["SeriesID"],)
-        ).fetchall()
-        add_data["Authors"] = ",".join([author["AuthorName"] for author in authors])
 
-        add_data["volumes"] = db.execute(
-            """
-            SELECT
-                BookID,
-                Title,
-                COALESCE(PublicationDate, "") AS PublicationDate,
-                Locations.LocationName AS LocationName,
-                COALESCE(ISBN13, ISBN10, "") AS ISBN
-            FROM Books
-            JOIN Locations ON Books.LocationID = Locations.LocationID
-            WHERE SeriesID = ?
-            ORDER BY Title;
-            """, (series_list[i]["SeriesID"],)
-        ).fetchall()
+@bp.route("/")
+@login_required
+def index():
+    """_summary_
+    登録書籍を一覧表示するview
 
-        parms = {
-            "title":series_data["SeriesName"],
-            "seriesName":series_data["SeriesName"],
-            "publisher":series_data["PublisherName"],
-            "author":add_data["Authors"],
-            "Location":add_data["Locations"][0]["LocationName"]
-        }
-        add_data["add_volume_url"] = url_for('manager.register', **parms)
+    Returns
+    -------
+    検索条件がなければ全ての書籍を表示
+    検索条件があれば検索条件にあった書籍のみを表示
+    """
 
-        series_list[i].update(add_data)
-    
+    db = get_db()
+    UserID = g.user["UserID"]
+
+    pamrs = get_url_parameters()
+
+    Books = get_have_books(db, UserID, pamrs)
+
+@bp.route("/index_series")
+@login_required
+def index_series():
+    """Overview
+    本をシリーズごとに表示
+
+    Returns
+    -------
+    検索条件がある場合、検索条件に一致する本を表示
+    検索条件がない場合、ユーザーが持つ本を表示
+    """
+    db = get_db()
+    UserID = g.user["UserID"]
+
+    parms = get_url_parameters()
+
+
+    plus_page = {**request.args}
+    minus_page = {**request.args}
+    plus_page["Page"] = parms["page"]+1
+    minus_page["Page"] = parms["page"]-1 if parms["page"]>0 else 0
+
+    series_list = get_series(db, UserID, parms)
+
     return render_template("index.html",
                             series_list=series_list,
-                            plus_parms=plus_parms,
-                            minus_parms=minus_parms)
+                            plus_parms=plus_page,
+                            minus_parms=minus_page)
