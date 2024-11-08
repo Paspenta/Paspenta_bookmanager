e7ffd7708dd9299f266687c2f18b08ad808b31f2
Paspenta <paspenta@outlook.jp>
2024-11-08 16:41:47 +0900

    add test index pagenation

--- tests/test_index.py
+++ tests/test_index.py
@@ -50,3 +50,39 @@ def test_index_Series(client, auth, parm, value, HitBook):
     response = client.get("/", query_string={parm:value})
     assert b"TestBook" not in response.data
     assert HitBook in response.data
+
+
+def test_index_pagenation(client, auth, app):
+    with app.app_context():
+        auth.login()
+        db = get_db()
+
+        for i in range(1, 62):
+            db.execute(
+                """
+                INSERT INTO Books (Title, SeriesID, LocationID, UserID)
+                VALUES (?, 1, 1, 1):
+                """, (f"Pagenation{i}",)
+            )
+        db.commit()
+
+        response = client.get("/", query_string={"Title":"Pagenation"})
+        data = response.data.decode("utf-8")
+        for i in range(1, 31):
+            assert f"Pagenation{i}" in data
+        for i in range(31, 62):
+            assert f"Pagenation{i}" not in data
+
+        response = client.get("/"), query_string=({"Title":"Pagenation", "page":"2"})
+        data = response.data.decode("utf-8")
+        for i in range(1, 31):
+            assert f"Pagenation{i}" not in data
+        for i in range(31, 61):
+            assert f"Pagenation{i}" in data
+        assert f"Pagenation61" not in data
+
+        response = client.get("/"), query_string=({"Title":"Pagenation", "page":"3"})
+        data = response.data.decode("utf-8")
+        for i in range(1, 61):
+            assert f"Pagenation{i}" not in data
+        assert f"Pagenation{i}" in data
