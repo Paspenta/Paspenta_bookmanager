53f36e681d9dcd77b36651997840e40ac259f46e
Paspenta <paspenta@outlook.jp>
2024-11-08 20:22:15 +0900

    add test book_edit

--- tests/test_edit.py
+++ tests/test_edit.py
@@ -1,3 +1,46 @@
 import pytest
 from bookmanager.db import get_db
 response = ""
+
+def test_book_edit(client, auth, app):
+    auth.login()
+    data={
+        "BookID":1,
+        "Title":"afterTitle",
+        "SeriesName":"afterSeries",
+        "PublisherName":"afterPublisher",
+        "AuthorName":"AfterAuthor",
+        "LocationName":"afterLocation",
+        "PUblicationDate":"after-date",
+        "ISBN10":"afterISBN10",
+        "ISBN13":"afterISBN13"
+    }
+
+    response = client.post("/book_edit", data=data)
+    assert response.headers["Location"] == "/"
+
+    with app.app_context():
+        db = get_db()
+        after_book = db.execute(
+            """
+            SELECT
+                Books.BookID AS BookID,
+                Title,
+                Series.SeriesName AS SeriesName,
+                Publishers.PublisherName AS PublisherName,
+                Locations.LocationName AS LocationName,
+                PublicationDate,
+                ISBN10,
+                ISBN13
+            FROM Books
+            JOIN Series ON Books.SeriesID = Series.SeriesID
+            JOIN Locations ON Books.LocationID = Locations.LocationID
+            LEFT JOIN BookAuthors.BookID = Books.BookID
+            LEFT JOIN Authors ON Authors.AuthorID, BookAuthors.AuthorID
+            LEFT JOIN Publishers ON Publishers.PublisherID, Publishers.PublisherID
+            WHERE Books.BookID = 1;
+            """
+        ).fetchone()
+        assert after_book is not None
+        for key, v in data.items():
+            assert after_book[key] == v
