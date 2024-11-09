5e3849a5ebe7b8c8e466b1128e34162a7da1712c
Paspenta <paspenta@outlook.jp>
2024-11-09 19:49:14 +0900

    fix db setattr

--- tests/test_db.py
+++ tests/test_db.py
@@ -7,13 +7,13 @@ def test_get_close_db(app):
     # get_dbはdbが閉じられるまで同じ接続を返す
     with app.app_context():
         db = get_db()
-        assert db is get_db
-    
+        assert db is get_db()
+
     # 既に閉じられているはずのdbからSELECTを実行
     # ProgrammingErrorが出なければテスト失敗
     with pytest.raises(sqlite3.ProgrammingError) as e:
         db.execute("SELECT 1")
-    
+
     # エラーメッセージにclosedが含まれるか
     assert "closed" in str(e.value)
 
@@ -21,13 +21,13 @@ def test_init_db_command(runner, monkeypatch):
     # 実行されたかを記録する
     class Recorder(object):
         called = False
-    
+
     # init_dbが呼び出されたことを記録
     def fake_init_db():
         Recorder.called = True
-    
+
     # init_dbが呼び出された時、fake_init_dbも実行する
-    monkeypatch.setattr("flask.db.init_db", fake_init_db)
+    monkeypatch.setattr("bookmanager.db.init_db", fake_init_db)
     result = runner.invoke(args=["init-db"]) # ターミナルでinit-dbを実行
 
     # init_db_cmdはinit_dbが終わった後、初期化成功メッセージを返すはず
