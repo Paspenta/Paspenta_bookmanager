e5a34f277c7a99d6dda25d200754f30d18d0f3ec
Paspenta <paspenta@outlook.jp>
2024-11-09 15:20:28 +0900

    add test register

---
+++ tests/test_register.py
@@ -0,0 +1,69 @@
+import pytest
+from bookmanager.db import get_db
+from flask import session, url_for
+
+
+def test_register(client, auth, app):
+    """_summary_
+    正常に登録できるか
+    """
+    auth.login()
+    data={
+        "Title":"NewTitle",
+        "Series":"NewSeries",
+        "Location":"NewLocation",
+        "author":"NewAuthor",
+        "Publisher":"NewPublisher",
+        "ISBN13":"New13",
+        "ISBN10":"New10"
+    }
+    parms = {
+        "Title":"TestTitle",
+        "Series":"TestSeries",
+        "author":"TestAuthor",
+        "Location":"TestLocation",
+        "Publisher":"TestPublisher",
+        "PublicationData":"TestData",
+        "ISBN13":"Test13",
+        "ISBN10":"Test10"
+    }
+    # urlを生成
+    url = "/register?"
+    flag = True
+    for key, v in parms.items():
+        if flag:
+            url = url + key + "=" + v
+            flag = False
+        else:
+            url = url + "&" + key + "=" + v
+
+    # URLパラメタがform valueに反映されるか
+    response = client.get(url)
+    html = response.data.decode("utf-8")
+    for v in parms.values():
+        assert v in html
+
+    response = client.post("/register", data=data)
+    assert response.headers["Location"] == url
+    with app.app_context():
+        db = get_db()
+        register_book = db.execute(
+            """
+            SELECT 1
+            FROM Books
+            JOIN Series ON Series.SeriesID = Books.SeriesID
+            JOIN LOcations ON Locations.LocationID = Books.LocationID
+            LEFT JOIN Publishers ON Publishers.PUblisherID = Books.PublisherID
+            LEFT JOIN BookAuthors ON Books.BookID = BookAuthors.BookID
+            LEFT JOIN Authors ON Authors.AuthorID = BookAuthors.AuthorID
+            WHERE
+                Books.Title = 'NewTitle'
+                AND Series.SeriesName = 'NewSeries'
+                AND Locations.LocationName = 'NewLocation'
+                AND Authors.AuthorName = 'NewAuthor'
+                AND Publishers.PublisherName = 'NewPublisher'
+                AND ISBN13 = 'New13'
+                AND ISBN10 = 'New10'
+            """
+        ).fetchone()
+        assert register_book is not None
