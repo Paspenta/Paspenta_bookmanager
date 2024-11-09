197a88920fe5cff36f521237c04cb7cadddb7f1d
Paspenta <paspenta@outlook.jp>
2024-11-09 16:49:21 +0900

    fix setattr request -> requests

--- tests/test_get_books.py
+++ tests/test_get_books.py
@@ -35,7 +35,7 @@ def test_get_books(monkeypatch, q, intitle, inauthor, isbn, page, flag):
     def fake_parse(book):
         return True
 
-    monkeypatch.setattr("request.get", fake_get)
+    monkeypatch.setattr("requests.get", fake_get)
     monkeypatch.setattr("bookmanager.get_books.parse_book", fake_parse)
     get_books(q, intitle, inauthor, isbn, page)
     assert Recorder.called == flag == Recorder.called_parse
