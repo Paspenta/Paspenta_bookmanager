455a051c54f2f32ca05cb4e5c4bceda705f38c0a
Paspenta <paspenta@outlook.jp>
2024-11-09 09:08:32 +0900

    add test book_edit get

--- tests/test_edit.py
+++ tests/test_edit.py
@@ -22,6 +22,16 @@ def test_book_edit(client, auth, app, flag, AuthorName, PublisherName, SeriesNam
         data["Authors"] = AuthorName
         data["PublisherName"] = PublisherName
 
+    response = client.get("/book_edit?BookID=1")
+    html = response.data.decode("utf-8")
+    book_datas = [
+        "TestBook1", "TestSeries", "TestPublisher",
+        "TestAuthor1", "TestLocation",
+        "xxxx-xx-xx", "1234567890123", "1234567890"
+    ]
+    for book_data in book_datas:
+        assert book_data in html
+
     response = client.post("/book_edit", data=data)
     assert response.headers["Location"] == "/"
 
@@ -55,3 +65,30 @@ def test_book_edit(client, auth, app, flag, AuthorName, PublisherName, SeriesNam
             assert after_book["SeriesName"] == "AfterTitle"
             assert after_book["PublisherName"] is None
             assert after_book["Authors"] is None
+
+
+@pytest.mark.parametrize(
+    ("BookID", "status_code"),
+    (2, 404),
+    (404, 404)
+)
+def test_book_edit_validate(client, auth, BookID, status_code):
+    auth.login()
+
+    data = {"BookID":1}
+
+    assert client.post("/book_edit").status_code == 400
+    assert client.get("/book_edit").status_code == 400
+
+    response = client.post("/book_edit", data=data)
+    html = response.data.decode("utf-8")
+    assert "タイトルがありません" in html
+
+    data["Title"] = "validate_title"
+    response = client.post("/book_edit", data=data)
+    html = response.data.decode("utf-8")
+    assert "本の場所が入力されていません" in html
+
+    data["BookID"] = BookID
+    assert client.post("/book_edit", data=data).status_code == status_code
+    assert client.get(f"/book_edit?BookID={BookID}").status_code == status_code
